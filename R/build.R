1df1cafaee439888dc592b6b09b327bbefeee397
Your Name <maibide@gmail.com>
2017-07-18 12:49:36 +0200

    add loadeR

--- R/build.R
+++ R/build.R
@@ -1,4 +1,4 @@
-
+library(drat)
 # Install drat and git2r
 # install.packages(c("drat","git2r"))
 
@@ -6,14 +6,16 @@
 # drat::initRepo(name = "datarepo", basepath = "/media/")
 # 
 # # Store the basepath + name
-options(dratRepo = "/media/maialen/work/WORK/prueba/datarepo/")
+options(dratRepo = getwd())
 # # Using this will disable the need to enter "repoDir"
 # # I've left it in just to illustrate...
 
+# Create packages from terminal: R CMD build <pck>; R CMD check <pkg>
+
 # Add a package to it
-drat::insertPackage("/media/maialen/work/WORK/GIT/transformeR_0.0-14.tar.gz",  # Path to src 
-                    repodir = "/media/maialen/work/WORK/prueba/datarepo/",                   # Location of git repo: not need if dratRepo set
-                    action="prune",                                  # Remove old package version
+drat::insertPackage("/media/maialen/work/WORK/GIT/loadeR_1.0.9.tar.gz",  # Path to src 
+                    repodir = "/media/maialen/work/WORK/GIT/climate4R/",                   # Location of git repo: not need if dratRepo set
+                    action = "prune",                                  # Remove old package version
                     commit = T)                                      # Commit to repo
 
 # (Optional) Remove old packages from repo at a later time
@@ -22,7 +24,7 @@ drat::insertPackage("/media/maialen/work/WORK/GIT/transformeR_0.0-14.tar.gz",  #
 ## Push Repository onto GitHub
 
 # Open repository
-repo = git2r::repository("/media/maialen/work/WORK/prueba/datarepo/")
+repo = git2r::repository("/media/maialen/work/WORK/GIT/climate4R/")
 
 # Authorize (not secure, need SSH key)
 cred = git2r::cred_user_pass("miturbide", "lukinvela9&9")
@@ -31,7 +33,7 @@ cred = git2r::cred_user_pass("miturbide", "lukinvela9&9")
 git2r::push(repo, "origin", "refs/heads/master", credentials = cred)
 
 # Add the repository to local R session for use with install.packages()
-drat::addRepo("datarepo","http://miturbide.github.io/datarepo")
+drat::addRepo("climate4R","http://miturbide.github.io/climate4R")
 drat::addRepo("datarepo","http://github.com/miturbide/datarepo")
 
 # ~$ git pull 
@@ -40,7 +42,8 @@ drat::addRepo("datarepo","http://github.com/miturbide/datarepo")
 # ~$ git push origin master
 
 setRepositories()
-install.packages("downscaleR")
+
+install.packages("downscaleR", repos = climate4R)
 install.packages("transformeR")
 library(transformeR)
 install.packages("face")
@@ -48,6 +51,6 @@ setRepositories()
 
 ?install.packages
 
-
+#create package for windows
 setwd("/media/maialen/work/WORK/GIT/mopa/")
 tools::write_PACKAGES(".", type="win.binary")
