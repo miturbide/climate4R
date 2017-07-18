bc0ab84dddf309108846378a2e2c1c5670d2f557
Your Name <maibide@gmail.com>
2017-07-18 13:03:51 +0200

    update transformeR

--- R/build.R
+++ R/build.R
@@ -6,15 +6,16 @@ library(drat)
 # drat::initRepo(name = "datarepo", basepath = "/media/")
 # 
 # # Store the basepath + name
-options(dratRepo = getwd())
+# options(dratRepo = getwd())
 # # Using this will disable the need to enter "repoDir"
 # # I've left it in just to illustrate...
 
 # Create packages from terminal: R CMD build <pck>; R CMD check <pkg>
 
 # Add a package to it
-drat::insertPackage("/media/maialen/work/WORK/GIT/loadeR_1.0.9.tar.gz",  # Path to src 
-                    repodir = "/media/maialen/work/WORK/GIT/climate4R/",                   # Location of git repo: not need if dratRepo set
+pkg <- "/media/maialen/work/WORK/GIT/transformeR_0.0.14.tar.gz"
+drat::insertPackage(pkg,  # Path to src 
+                    repodir = getwd(),                   # Location of git repo: not need if dratRepo set
                     action = "prune",                                  # Remove old package version
                     commit = T)                                      # Commit to repo
 
@@ -24,26 +25,26 @@ drat::insertPackage("/media/maialen/work/WORK/GIT/loadeR_1.0.9.tar.gz",  # Path 
 ## Push Repository onto GitHub
 
 # Open repository
-repo = git2r::repository("/media/maialen/work/WORK/GIT/climate4R/")
+repo = git2r::repository(getwd())
 
 # Authorize (not secure, need SSH key)
 cred = git2r::cred_user_pass("miturbide", "lukinvela9&9")
 
 # Push changes in local repository to GitHub
-git2r::push(repo, "origin", "refs/heads/master", credentials = cred)
+git2r::push(repo, "origin", "refs/heads/gh-pages", credentials = cred)
 
 # Add the repository to local R session for use with install.packages()
-drat::addRepo("climate4R","http://miturbide.github.io/climate4R")
-drat::addRepo("datarepo","http://github.com/miturbide/datarepo")
+# drat::addRepo("climate4R","http://miturbide.github.io/climate4R")
 
 # ~$ git pull 
 # ~$ git checkout master
 # ~$ git merge devel
 # ~$ git push origin master
 
-setRepositories()
+# setRepositories()
 
-install.packages("downscaleR", repos = climate4R)
+install.packages("loadeR", repos = "http://miturbide.github.io/climate4R")
+
 install.packages("transformeR")
 library(transformeR)
 install.packages("face")
